5a8b8f9330a820d51903b6c6bc08cfab90584fba
Adhwapw <pranajaadhwa@gmail.com>
2023-10-13 16:24:39 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "------------------------------------------" << endl;
+    cout << "Nama : Adhwa Pranaja Widyadana" << endl;
+    cout << "NPM : 231063117061" << endl <<endl;
+    cout << "Kelas : 1A Informatika" << endl << endl;
+    cout << "Cita-cita : Ingin Menjadi presiden" << endl;
+    cout << "Motivasi Masuk Informatika : Kepencet" << endl;
+    cout << "------------------------------------------" << ends;
+    return 0;
+}
